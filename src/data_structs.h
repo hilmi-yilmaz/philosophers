21fcb788b51104ba25f0420b149b8cad468c86c0
Hilmi Yilmaz <hilmiyilmaz.1998@gmail.com>
2022-08-24 17:32:14 +0200

    chore: remove unnecessary comments

--- src/data_structs.h
+++ src/data_structs.h
@@ -6,7 +6,7 @@
 /*   By: hyilmaz <hyilmaz@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/07 17:00:42 by hyilmaz       #+#    #+#                 */
-/*   Updated: 2022/08/18 15:12:11 by hyilmaz       ########   odam.nl         */
+/*   Updated: 2022/08/24 17:29:01 by hyilmaz       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,6 @@
 # include <stddef.h>
 # include <pthread.h>
 # include <stdbool.h>
-
-/* User defined headers */
-
 
 /* Macros */
 # define RED "\033[0;31m"
@@ -49,40 +46,6 @@ typedef struct s_input_data
 	size_t			time_to_sleep;
 	int				number_of_times_to_eat;
 }		t_input_data;
-
-// typedef struct s_philo_data
-// {
-
-// 	// Philosopher ID
-// 	size_t			philo_id;
-
-// 	// Pointer to input data
-// 	t_input_data	*input_data;
-
-// 	// Is the philosopher dead?
-// 	bool			*is_dead;
-// 	pthread_mutex_t	*mutex_is_dead;
-
-// 	// How many times has the philo already eaten?
-// 	int				*times_ate;
-// 	pthread_mutex_t	*mutex_times_ate;
-
-// 	// Forks
-// 	pthread_mutex_t	*forks;
-// 	pthread_mutex_t	*right_fork;
-// 	pthread_mutex_t	*left_fork;
-
-// 	// Starting time
-// 	t_milliseconds	simulation_start_time;
-
-// 	// Mutex for checking when the philo last ate
-// 	t_milliseconds	*timestamp_last_meal;
-// 	pthread_mutex_t	*mutex_timestamp_last_meal;
-
-// 	// Mutex for printing messages to the console
-// 	pthread_mutex_t	*mutex_print;
-// }		t_philo_data;
-
 
 typedef struct s_philo
 {
